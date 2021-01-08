7f11eb28535577daa198354f791ce897251c73c3
Tyler P Farris <Tylerpfarris@gmail.com>
2021-01-07 16:04:14 -0800

    calculator first draft

--- app.js
+++ app.js
@@ -3,3 +3,52 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const addButton = document.getElementById('add-button')
+const subtractButton = document.getElementById('subtract-button')
+const multiplyButton = document.getElementById('multiply-button')
+const divideButton = document.getElementById('divide-button')
+const Field1 = document.getElementById('input-1')
+const Field2 = document.getElementById('input-2')
+const sum = document.getElementById('sumField')
+
+addButton.addEventListener('click', () => {
+
+  const fieldValue1 = Field1.valueAsNumber;
+  const fieldValue2 = Field2.valueAsNumber;
+
+  const sum = fieldValue1 + fieldValue2;
+
+  sumField.textContent = sum;
+});
+
+subtractButton.addEventListener('click', () => {
+
+  const fieldValue1 = Field1.valueAsNumber;
+  const fieldValue2 = Field2.valueAsNumber;
+
+  const sum = fieldValue1 - fieldValue2;
+
+  sumField.textContent = sum;
+});
+
+multiplyButton.addEventListener('click', () => {
+
+  const fieldValue1 = Field1.valueAsNumber;
+  const fieldValue2 = Field2.valueAsNumber;
+
+  const sum = fieldValue1 * fieldValue2;
+
+  sumField.textContent = sum;
+});
+
+divideButton.addEventListener('click', () => {
+
+  const fieldValue1 = Field1.valueAsNumber;
+  const fieldValue2 = Field2.valueAsNumber;
+
+  const sum = fieldValue1 / fieldValue2;
+
+  sumField.textContent = sum;
+});
+
