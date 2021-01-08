f0bb6bf958a389318f0882c27d180e0482ba8642
Tyler P Farris <Tylerpfarris@gmail.com>
2021-01-08 13:44:41 -0800

    calculator w/ test

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,126 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+
+import { getAdditionSum, getSubtractionSum, getMultiplicationSum, getDivisionSum } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+// getAdditionSum test
+
+test('It should return 5 when provided 3 and 2', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const expected = 5;
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = getAdditionSum(3, 2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('It should return -5 when provided -10 and 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = -5;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getAdditionSum(-10, 5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+// getSubtractionSum test
+
+test('It should return 1 when provided 2 and 1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 1;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getSubtractionSum(2, 1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+
+test('It should return -1 when provided 1 and 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = -1;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getSubtractionSum(1, 2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+// getMultiplicationSum test
+
+test('It should return 2 when provided 1 and 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 2;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getMultiplicationSum(1, 2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('It should return -6 when provided -3 and 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = -6;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getMultiplicationSum(-3, 2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+// getDivisionSum Test
+
+test('It should return -6 when provided -12 and 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = -6;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getDivisionSum(-12, 2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('It should return 6 when provided 12 and 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 6;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getDivisionSum(12, 2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
--- utils.js
+++ utils.js
@@ -5,13 +5,13 @@ export function getAdditionSum(num1, num2) {
 };
 
 export function getSubtractionSum(num1, num2) {
-    return num1 + num2;
+    return num1 - num2;
 };
 
 export function getMultiplicationSum(num1, num2) {
-    return num1 + num2;
+    return num1 * num2;
 };
 
 export function getDivisionSum(num1, num2) {
-    return num1 + num2;
+    return num1 / num2;
 };
